02427fd6cc9a8e19bea49a7a65f8604149a51c4b
Unaomi <118616850+Unaomi@users.noreply.github.com>
2023-02-06 22:11:36 -0600

    Car servincing features added

--- app.py
+++ app.py
@@ -1,2 +1,8 @@
 # I am watching video 1
 # Bugs removed
+print ("car services automated")
+print ("simon")
+print ("chidi")
+# echo simon
+print ("LSS")
+print ("hello $naomi your car need serving")
